9ce0a3117d65b41cd9513bb74325af1395f613e0
JPatel87 <jaime_patel@hotmail.co.uk>
2022-09-07 12:58:39 +0000

    Add bouquet app models

--- bouquets/models.py
+++ bouquets/models.py
@@ -1,3 +1,40 @@
+"""Imports from django."""
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Category model for bouquets.
+    """
+
+    class Meta:
+        """
+        Method to display category model as categories in admin view.
+        """
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Bouquet(models.Model):
+    """
+    Bouquet model for bouquets.
+    """
+    category = models.ForeignKey(
+        'Category', null=True,
+        blank=True, on_delete=models.SET_NULL
+    )
+    sku = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """
+        Method to display bouquet instance by its name.
+        """
+        return str(self.name)
