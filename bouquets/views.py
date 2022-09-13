dcbe25ca34df96b5062fa09f9b19da2341e20e73
JPatel87 <jaime_patel@hotmail.co.uk>
2022-09-13 19:53:49 +0000

    Add search functionality

--- bouquets/views.py
+++ bouquets/views.py
@@ -1,19 +1,32 @@
 """Imports from django and the bouquet model."""
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Bouquet
 
 
 def all_bouquets(request):
     """ Function to view all bouquets.
 
-    Sorting and search queries will also
-    be included.
+    Sorting and search queries also
+    included.
     """
 
     bouquets = Bouquet.objects.all()
+    query = None
 
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('bouquets'))
+            
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        bouquets = bouquets.filter(queries)
+    
     context = {
         'bouquets': bouquets,
+        'search_term': query,
     }
 
     return render(request, 'bouquets/bouquets.html', context)
