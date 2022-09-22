dc39cecb19c7872712900297a4f822938e04b07c
JPatel87 <jaime_patel@hotmail.co.uk>
2022-09-22 23:16:26 +0000

    Add checkout view and templates

--- blossoming_bouquets/urls.py
+++ blossoming_bouquets/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('bouquets/', include('bouquets.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
