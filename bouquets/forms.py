b3ea59a171c12fe4413dadc324a03cc12046856f
JPatel87 <jaime_patel@hotmail.co.uk>
2022-09-29 19:49:06 +0000

    Add image widget for edit proeuct page

--- bouquets/forms.py
+++ bouquets/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Bouquet
 
 
@@ -7,6 +8,8 @@ class BouquetForm(forms.ModelForm):
     class Meta:
         model = Bouquet
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
