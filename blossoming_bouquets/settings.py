b2aba036bfff2e0708c1c74a857f45e6ac87980c
JPatel87 <jaime_patel@hotmail.co.uk>
2022-10-01 11:16:18 +0000

    Creat heroku app and dump categories and bouquet data to Heroku Postgress

--- blossoming_bouquets/settings.py
+++ blossoming_bouquets/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,6 @@ SECRET_KEY = 'django-insecure-l4rm7hx0t*8el9)kro6e0^c&j6xm&h6ebkp*p2y9v(p4)gp*8a
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -115,17 +115,21 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'blossoming_bouquets.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
