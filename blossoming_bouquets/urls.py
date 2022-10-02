6e10dfbf426d589a0ffcd77840c34a4d44000d96
JPatel87 <jaime_patel@hotmail.co.uk>
2022-10-02 19:05:43 +0000

    Add 404 page

--- blossoming_bouquets/urls.py
+++ blossoming_bouquets/urls.py
@@ -1,4 +1,5 @@
 """Imports from django"""
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -14,3 +15,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'blossoming_bouquets.views.handler404'
