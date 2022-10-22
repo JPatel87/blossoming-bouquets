67050c0b7fa2ecd94f83153b68748130dadb2aa9
JPatel87 <jaime_patel@hotmail.co.uk>
2022-10-22 20:03:08 +0000

    Add post model admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Post, Comment
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    Post model view on django admin for admin user.
+    """
+
+    list_display = (
+        'title',
+        'slug',
+        'date',
+    )
+
+admin.site.register(Post, PostAdmin)
