af38e1521a529b5d3d824f74c7b4e8f1a50ce048
JPatel87 <jaime_patel@hotmail.co.uk>
2022-11-02 18:11:28 +0000

    Update bouquet model with decimal validator

--- bouquets/models.py
+++ bouquets/models.py
@@ -1,5 +1,7 @@
 """ Imports from django. """
+from decimal import Decimal
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Category(models.Model):
@@ -28,7 +30,11 @@ class Bouquet(models.Model):
     sku = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('15.00')),
+                    MaxValueValidator(Decimal('100.00'))])
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
